ed44ebdd49fa198b344a6a7e5de84515fe562652
Cetiia <114244531+cetiiadev@users.noreply.github.com>
2023-05-24 15:24:41 -0500

    Test geolocalization

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,24 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 Route::get('/activity-log', function () {
+
     $logData = Storage::get('logs/activity.csv');
     $logRows = explode("\n", $logData);
     $logs = [];
 
     foreach ($logRows as $row) {
         $logFields = explode(',', $row);
-        $logs[] = [
-            'user' => $logFields[0],
-            'ip' => $logFields[1],
-            'path' => $logFields[2],
-            'method' => $logFields[3],
-            'date' => $logFields[4],
-            'time' => $logFields[5],
-            'browser' => $logFields[6],
-        ];
+        if (count($logFields)>1) {
+            $logs[] = [
+                'user' => $logFields[0],
+                'ip' => $logFields[1],
+                'path' => $logFields[2],
+                'method' => $logFields[3],
+                'date' => $logFields[4],
+                'time' => $logFields[5],
+                'browser' => $logFields[6],
+            ];
+        }
     }
 
     return view('laravel-activity-log::activity-log', compact('logs'));
--- src/Middleware/LaravelActivityLogMiddleware.php
+++ src/Middleware/LaravelActivityLogMiddleware.php
@@ -4,24 +4,30 @@ namespace Cetiia\LaravelActivityLog\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Http;
 
 class LaravelActivityLogMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $logData = [
-            'user' => $request->user() ? $request->user()->name : 'anonymous',
-            'ip' => $request->ip(),
-            'path' => $request->path(),
-            'method' => $request->method(),
-            'date' => date('Y-m-d'),
-            'time' => date('H:i:s'),
-            'browser' => $request->header('User-Agent'),
-        ];
-    
-        // Save log as CSV
-        $csvData = implode(',', $logData);
-        Storage::append('logs/activity.csv', $csvData);
+        
+        if ($request->ip() != '127.0.0.1') {
+            $data = Http::get('http://ip-api.com/json/' . $request->ip())->json();
+            dd($data);
+            $logData = [
+                'user' => $request->user() ? $request->user()->name : 'anonymous',
+                'ip' => $request->ip(),
+                'path' => $request->path(),
+                'method' => $request->method(),
+                'date' => date('Y-m-d'),
+                'time' => date('H:i:s'),
+                'browser' => $request->header('User-Agent'),
+            ];
+        
+            // Save log as CSV
+            $csvData = implode(',', $logData);
+            Storage::append('logs/activity.csv', $csvData);
+        }
         return $next($request);
     }
 }
