b69d9a56bd5415a3302a0edd3d257e44f3658286
Cetiia <114244531+cetiiadev@users.noreply.github.com>
2023-05-25 09:18:24 -0500

    test download xls

---
+++ src/Models/Log.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Cetiia\LaravelActivityLog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Log extends Model
+{
+    protected $fillable = [
+        'user',
+        'ip',
+        'country',
+        'state',
+        'city',
+        'path',
+        'method',
+        'date',
+        'time',
+        'browser',
+    ];
+
+    public static function records()
+    {
+        $logData = Storage::get('logs/activity.csv');
+        $logRows = explode("\n", $logData);
+        $logs = [];
+
+        foreach ($logRows as $row) {
+            $logFields = explode(',', $row);
+            if (count($logFields) > 1) {
+                $logs[] = [
+                    'user' => $logFields[0],
+                    'ip' => $logFields[1],
+                    'country' => $logFields[2],
+                    'state' => $logFields[3],
+                    'city' => $logFields[4],
+                    'path' => $logFields[5],
+                    'method' => $logFields[6],
+                    'date' => $logFields[7],
+                    'time' => $logFields[8],
+                    'browser' => $logFields[9],
+                ];
+            }
+        }
+        return $logs;
+    }
+}
