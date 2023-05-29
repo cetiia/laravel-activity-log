d26c59ed0a3f674ee2ae8460e87ef5c67e6d9111
Cetiia <114244531+cetiiadev@users.noreply.github.com>
2023-05-29 08:34:43 -0500

    Undo sort order and add desc sorting

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 Route::get('/activity-log', function () {
     if (Schema::hasTable('logs')) {
-        $logs = Log::get()->sortBy('date')->sortBy('time');
+        $logs = Log::get()->sortBysortByDesc('time')->sortByDesc('date');
     } else {
         // Table does not exist
         $logs = [];
