9783e8f5bd4116ff2252929c74a1fb7602cc7093
Cetiia <114244531+cetiiadev@users.noreply.github.com>
2023-05-29 08:48:22 -0500

    change auth middleware group for gate authorize

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 
 use Cetiia\LaravelActivityLog\Models\Log;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 
-Route::middleware('auth')->group(function () {
-    Route::get('/activity-log', function () {
-        if (Schema::hasTable('logs')) {
-            $logs = Log::get()->sortByDesc('time')->sortByDesc('date');
-        } else {
-            // Table does not exist
-            $logs = [];
-        }
-        return view('laravel-activity-log::activity-log', compact('logs'));
-    })->name('activity-log');
-});
+Route::get('/activity-log', function () {
+    if (Gate::has('activity-log')) {
+        Gate::authorize('activity-log');
+    }
+    if (Schema::hasTable('logs')) {
+        $logs = Log::get()->sortByDesc('time')->sortByDesc('date');
+    } else {
+        // Table does not exist
+        $logs = [];
+    }
+    return view('laravel-activity-log::activity-log', compact('logs'));
+})->name('activity-log');
