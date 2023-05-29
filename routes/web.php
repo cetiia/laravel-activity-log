f672c9630077cdb5727b9dae7500fa580f0f76cf
Cetiia <114244531+cetiiadev@users.noreply.github.com>
2023-05-29 08:36:51 -0500

    Fix typo

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 Route::get('/activity-log', function () {
     if (Schema::hasTable('logs')) {
-        $logs = Log::get()->sortBysortByDesc('time')->sortByDesc('date');
+        $logs = Log::get()->sortByDesc('time')->sortByDesc('date');
     } else {
         // Table does not exist
         $logs = [];
