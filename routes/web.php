416cec598a9395c1d27c3b43e4c11b99d4570cbd
Cetiia <114244531+cetiiadev@users.noreply.github.com>
2023-05-29 08:42:08 -0500

    Add route middleware group

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,15 @@
 use Cetiia\LaravelActivityLog\Models\Log;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
-Route::get('/activity-log', function () {
-    if (Schema::hasTable('logs')) {
-        $logs = Log::get()->sortByDesc('time')->sortByDesc('date');
-    } else {
-        // Table does not exist
-        $logs = [];
-    }
-    return view('laravel-activity-log::activity-log', compact('logs'));
-})->name('activity-log');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/activity-log', function () {
+        if (Schema::hasTable('logs')) {
+            $logs = Log::get()->sortByDesc('time')->sortByDesc('date');
+        } else {
+            // Table does not exist
+            $logs = [];
+        }
+        return view('laravel-activity-log::activity-log', compact('logs'));
+    })->name('activity-log');
+});
